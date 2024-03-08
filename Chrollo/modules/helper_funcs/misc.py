2a9cff05583711a24c45fc3fa7034727012ed05e
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-08 15:05:34 +0530

    Update misc.py

--- Chrollo/modules/helper_funcs/misc.py
+++ Chrollo/modules/helper_funcs/misc.py
@@ -89,7 +89,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "‹", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("ˆModulesˆ"),
+                EqInlineKeyboardButton("ˆModulesˆ", callback_data=""),
                 EqInlineKeyboardButton(
                     "›", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
