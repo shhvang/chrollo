b93cb0b5394c2891c6f46fc51686f727f2302a51
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-08 15:23:20 +0530

    Update misc.py

--- Chrollo/modules/helper_funcs/misc.py
+++ Chrollo/modules/helper_funcs/misc.py
@@ -49,56 +49,70 @@ def split_message(msg: str) -> List[str]:
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
-        modules = sorted(
+            modules = sorted(
             [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.lower()
+                        prefix, x.__mod_name__.replace(" ", "_").lower()
                     ),
                 )
                 for x in module_dict.values()
             ]
         )
     else:
-        modules = sorted(
-            [
+            modules = sorted(
+                 [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.lower()
+                        prefix, chat, x.__mod_name__.replace(" ", "_").lower()
                     ),
                 )
                 for x in module_dict.values()
             ]
+             )
+
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
+    for m in pairs:
+        for _ in m:
+            i += 1
+    if len(modules) - i == 1:
+        pairs.append((modules[-1],))
+    elif len(modules) - i == 2:
+        pairs.append(
+            (
+                modules[-2],
+                modules[-1],
+            )
         )
 
-    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    COLUMN_SIZE = 6
 
-    round_num = len(modules) / 3
-    calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
-        pairs.append((modules[-1],))
-
-    max_num_pages = ceil(len(pairs) / 4)
+    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 3:
-        pairs = pairs[modulo_page * 4 : 4 * (modulo_page + 1)] + [
+    if len(pairs) > COLUMN_SIZE:
+        pairs = pairs[
+            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
+        ] + [
             (
                 EqInlineKeyboardButton(
-                    "‹", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "‹",
+                    callback_data="{}_prev({})".format(prefix, modulo_page),
                 ),
-                EqInlineKeyboardButton("ˆ Modules ˆ", callback_data="fallen_back"),
                 EqInlineKeyboardButton(
-                    "›", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "Back",
+                    callback_data="start_back",
+                ),
+                EqInlineKeyboardButton(
+                    "›",
+                    callback_data="{}_next({})".format(prefix, modulo_page),
                 ),
             )
         ]
-
-    else:
-        pairs += [[EqInlineKeyboardButton("Retreat", callback_data="fallen_back")]]
 
     return pairs
 
