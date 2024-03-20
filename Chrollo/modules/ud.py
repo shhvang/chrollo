2b03d944a97b703c69ac7183dfb8f5bc3487074b
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-20 10:48:13 +0530

    Update ud.py

--- Chrollo/modules/ud.py
+++ Chrollo/modules/ud.py
@@ -20,13 +20,14 @@ def ud(update: Update, context: CallbackContext):
           # Print the exception for debugging purposes
         reply_text = f"An error occurred while fetching the definition. {e}"
     
-    message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 def define(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     text = message.text[len("/define "):]
     results = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{text}").json()
+    message.reply_text(text)
     
     try:
         if results:
